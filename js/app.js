bd9e09575892cca0d2f34b7306758394d8f6fa6f
Jossue Renteria <jrenteria@unitedvirtualities.com>
2019-12-17 16:20:33 -0700

    LP-009-updates made

--- js/app.js
+++ js/app.js
@@ -1,48 +1,63 @@
 // Portfolio section start
-var pItems = document.getElementById("portfolioItems");
-var mItems = document.getElementById("portfolioModals");
-var portfolioItems = [];
-var modalItems = [];
+var pItems = document.getElementById("portfolioItems"), mItems = document.getElementById("portfolioModals");
+var portfolioItems = [], modalItems = [];
+
 var items = [
     {
         name: 'Pozole',
-        type: 'Caldo',
+        type: 'Soup',
         origin: 'México',
-        ingredients: 'Granos de maíz y carne',
-        desc: 'El pozole (del náhuatl pozolli, de tlapozonalli, «hervido» o «espumoso»,​ o del cahíta posoli, «cocer maíz») es un plato tradicional mesoamericano, un caldo hecho a base de granos de maíz de un tipo conocido comúnmente como cacahuazintle, a la que se agrega, según la región, carne de pollo o de cerdo como ingrediente secundario.1​ De esta preparación básica existen variaciones por todo el territorio mexicano y algunas cocinas de América Central. Por ejemplo, en México el pozole verde es del estado de Guerrero y el rojo de Sonora, Sinaloa, Nayarit, Guanajuato, Jalisco y Tlaxcala.2 A este versátil platillo se le puede acompañar con diversos ingredientes, que varían según la zona y costumbres. En América del Sur existe un plato con el cual guarda parecido, llamado patasca.'
+        spicy: 3,
+        ingredients: 'Pork, hominy (maíz pozolero), ancho chiles, guajillo chiles, onion, garlic, and Mexican oregano and optional chiles de arból.',
+        desc: 'Pozole (Nahuatl languages: pozolli Spanish pronunciation: [po\'sole], pozole), which means "hominy", is a traditional soup or stew from Mexican Cuisine. It is made from hominy, with meat (typically pork), and can be seasoned and garnished with shredded lettuce or cabbage, chile peppers, onion, garlic, radishes, avocado, salsa or limes.'
     },{
         name: 'Tacos',
-        type: 'Garnacha - Comida rápida',
+        type: 'Finger food',
         origin: 'México',
-        ingredients: 'Tortilla de maíz y carne',
-        desc: 'Un taco es una preparación mexicana que en su forma estándar consiste en una tortilla enrollada (generalmente de maíz, también de harina de trigo, de nopal o de algún otro ingrediente) que contiene algún alimento encima y algún tipo de salsa. Es considerado como uno de los platillos más representativos de la comida mexicana.'
+        spicy: 4,
+        ingredients: 'Tortillas, meat, vegetables.',
+        desc: 'A taco (US: /ˈtɑːkoʊ/, UK: /ˈtækoʊ/, Spanish: [ˈtako]) is a traditional Mexican dish consisting of a small hand-sized corn or wheat tortilla topped with a filling. The tortilla is then folded around the filling and eaten by hand. A taco can be made with a variety of fillings, including beef, pork, chicken, seafood, vegetables, and cheese, allowing great versatility and variety. They are often garnished with various condiments, such as salsa, guacamole, or sour cream, and vegetables, such as lettuce, onion, tomatoes, and chiles. Tacos are a common form of antojitos, or Mexican street food, which have spread around the world.'
     },{
         name: 'Chile en nogada',
-        type: 'Gurmet',
+        type: 'Stuffed vegetable',
         origin: 'México - Puebla',
-        ingredients: 'Chile poblano, guiso y granada roja',
-        desc: 'El chile en nogada (más comúnmente chiles en nogada, en plural, a pesar de que no suele servirse más que un único chile) es uno de los platillos típicos de la gastronomía del estado de Puebla. Ha sido llamado el "platillo poblano por excelencia".1​ Se prepara con chile poblano relleno de un guisado de picadillo y frutas, que incluye acitrón y que se cubre con salsa de nuez (nogada), perejil y granada. Los colores del chile poblano, de la crema y de la granada simboliza los de la bandera de México.'
+        spicy: 2,
+        ingredients: 'Poblano chile, picadillo, walnuts, cream, pomegranate.',
+        desc: 'Chiles en nogada is a Mexican dish of poblano chiles stuffed with picadillo (a mixture usually containing shredded meat, aromatics, fruits and spices) topped with a walnut-based cream sauce, called nogada, and pomegranate seeds, and typically served at room temperature.'
     },{
         name: 'Guacamole',
-        type: 'Salsa',
+        type: 'Sauce',
         origin: 'México',
-        ingredients: 'Aguacate, sal, limon, queso(opcional), verdura variada(opcional)',
-        desc: 'El guacamole o guacamol es una salsa mexicana preparada a base de aguacate o palta y chile verde.[cita requerida] El nombre guacamole proviene del náhuatl Ahuacamolli traducido al español, que se compone de las palabras Ahuacatl (aguacate) + molli (mole o salsa). El aguacate tenía una significación erótica para los aztecas, tanto que las mujeres no podían realizar la recolección de los frutos,[cita requerida] ya que simbolizaban los testículos.1​2​ Según la mitología prehispánica, Quetzalcoatl, el dios tolteca, ofreció la receta del guacamole a su pueblo, quienes la extendieron por el territorio de Mesoamérica.'
+        spicy: 3,
+        ingredients: 'Avocados, sea salt, lime juice',
+        desc: 'Guacamole (Spanish: [(ɡ)waka\ˈmole] (About this soundlisten), see below for more; informally shortened to guac in North America since the 1980s) is an avocado-based dip, spread, or salad first developed by the Aztecs in present-day Mexico. In addition to its use in modern Mexican cuisine, it has become part of international and American cuisine as a dip, condiment and salad ingredient.'
     },{
         name: 'Enchilada',
-        type: 'Comida corrida',
+        type: 'Street food',
         origin: 'México',
-        ingredients: 'Tortilla de maíz, pollo, verdura variada',
-        desc: 'La enchilada es un plato que en México se elabora con tortilla de maíz bañada en alguna salsa picante utilizando chile en su preparación. Dependiendo del estilo, la enchilada puede ir acompañada o rellena de carnes —pollo,pavo,res— o queso; además de ser acompañada de alguna guarnición adicional, que generalmente consiste en cebolla fresca picada o en rodajas, lechuga, crema de leche y queso.'
+        spicy: 4,
+        ingredients: 'Tortillas, chili pepper sauce, meat',
+        desc: 'An enchilada (/ˌɛntʃɪˈlɑːdə/, Spanish: [entʃiˈlaða]) is a corn tortilla rolled around a filling and covered with a savory sauce. Enchiladas can be filled with a variety of ingredients, including various meats, cheese, beans, potatoes, vegetables or combinations, a variety of sauces can also be used to cover the enchiladas, including chile-based sauces, such as salsa roja, various moles, or cheese-based sauces such as chile con queso. Originating in Mexico, enchiladas are a popular dish throughout Mexico and the American Southwest.'
     },{
         name: 'Sopes',
         type: 'Antojito',
         origin: 'México',
-        ingredients: 'Granos de maíz y carne',
-        desc: 'Los sopes, pellizcadas, picadas o picaditas son una comida mexicana, consistente de una Tortilla de maíz gruesa, originalmente frita con manteca, sobre la que se añaden diferentes ingredientes, como chicharrón o carne, además de queso, verduras y salsa picante, con variaciones regionales.'
+        spicy: 4,
+        ingredients: 'Corn dough, vegetables, meat, refried beans, cheese, lettuce, onions, red or green sauce (salsa)',
+        desc: 'A sope (Spanish pronunciation: [ˈso.pe]), also known as picadita (in Tierra Caliente, Guerrero) is a traditional Mexican dish originating in the central and southern parts of Mexico, where it was sometimes first known as pellizcadas. It is an antojito, which at first sight looks like an unusually thick tortilla with vegetables and meat toppings. The base is made from a circle of fried masa (ground maize soaked in lime, also used as the basis for tamales and tortillas) with pinched sides. This is then topped with refried beans and crumbled cheese, lettuce, onions, red or green sauce (salsa, made with chiles or tomatillos, respectively), and sour cream. Sometimes other ingredients (mostly meat) are also added to create different tastes and styles of sopes; they are roughly the size of a fist.'
     }
 
 ];
+var spicyLevel = function(level){
+    let levelArray = [];
+    
+    for(let i = 1; i <= level; i++){
+        levelArray.push(
+            `<i class="fas fa-pepper-hot"></i>`
+        )
+    }
+    return levelArray.join('');
+}
 
 for(var i = 0; i < items.length; i++){
     portfolioItems.push(`
@@ -75,16 +90,18 @@ for(var i = 0; i < items.length; i++){
                     <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">${items[i].name}</h2>
                     <!-- Icon Divider -->
                     <div class="divider-custom">
-                      
+                      <div class="divider-custom-line"></div>
                       <div class="divider-custom-icon">
                         <i class="fas fa-utensils"></i>
                       </div>
-                      
+                    <div class="divider-custom-line"></div>
                     </div>
                     <!-- Portfolio Modal - Image -->
                     <img class="img-fluid rounded mb-5" src="img/portfolio/${items[i].name.toLocaleLowerCase()}.jpg" alt="">
                     <!-- Portfolio Modal - Text -->
-                    <p class="mb-5 text-justify">Tipo: ${items[i].type}</p>
+                    <p class="text-justify">Type: ${items[i].type}</p>
+                    <p class="text-justify">Ingredients: ${items[i].ingredients}</p>
+                    <p class="text-justify">Spicy level(1-5): ${spicyLevel(items[i].spicy)}</p>
                     <p class="mb-5 text-justify">${items[i].desc}</p>
                     <button class="btn btn-primary" href="#" data-dismiss="modal">
                       <i class="fas fa-times fa-fw"></i>
@@ -97,10 +114,10 @@ for(var i = 0; i < items.length; i++){
           </div>
         </div>
       </div>
-        
         `
     );
 }
+
 pItems.innerHTML = portfolioItems.join('');
 mItems.innerHTML = modalItems.join('');
 
