93fd87d357d5e85c4efda27b69fc73784c300751
Jossue Renteria <jrenteria@unitedvirtualities.com>
2019-12-18 12:34:07 -0700

    LP-Final-Beauty code and fixed up

--- js/app.js
+++ js/app.js
@@ -122,3 +122,18 @@ pItems.innerHTML = portfolioItems.join('');
 mItems.innerHTML = modalItems.join('');
 
 // Portfolio section end
+
+$(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+              $('html, body').animate({
+                  scrollTop: (target.offset().top - 85)
+              }, 1000);
+              return false;
+          }
+      }
+  });
+});
